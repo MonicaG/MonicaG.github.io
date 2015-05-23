172ad01cfbce17bebf89cdc976fdfac541f2780d
Monica Granbois <mgranbois@outlook.com>
2015-05-23 15:06:57 -0700

    Got layout done

---
+++ about.md
@@ -0,0 +1,13 @@
+---
+layout: default
+title: About
+permalink: /about.html
+---
+#About
+
+The purpose of this blog is to help me clarify my thoughts and code.  The process of explaining a concept helps me achieve a better understanding of it.
+
+I am currently interested in Java 8 and Swift. 
+
+
+
---
+++ js/blog.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function () {
+    //highlights the selected menu link
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('active');
+});
+
+   
+
+   
+
