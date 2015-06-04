29591ccc6fafac1d41a745aa601e38430ae8c362
Monica Granbois <mgranbois@outlook.com>
2015-06-03 21:23:05 -0700

    Formatting for displaying code blocks

--- js/blog.js
+++ js/blog.js
@@ -6,7 +6,13 @@ $(document).ready(function () {
     $('ul.nav a').filter(function() {
          return this.href == url;
     }).parent().addClass('active');
+
+    //used for the pygments code highlighting.  The line numbers use a table.
+    //To make it responsive, the table needs to be wrapped in the 
+    //the table-responsive tag as per bootstrap documentation.
+    $( ".highlighttable" ).wrap( "<div class='table-responsive'/>");
 });
+
 
    
 
